eb305264decf5a32d99926c700d28846afa06256
Marlon Clay <luciousmc@icloud.com>
2021-06-02 15:28:03 -0700

    fixed folder structure

--- manga-house/src/components/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Header from "./header";
-import "./app.css";
-
-const App = () => {
-  return <Header />;
-};
-
-export default App;
--- v-stream-cat/src/views/Browse.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Browse</h1>
-</template>
